f5833d54afddf4fc6c8b972e9f26fb39f378dc22
Sagar Jain <thesagarjain8@gmail.com>
2026-07-14 17:56:32 +0530

    lecture 4 done

--- lecture3.sql
+++ lecture3.sql
@@ -74,4 +74,74 @@ VALUES (001, 'moxa', 01),
     (002, 'foxa', 03),
     (003, 'troxa', 02);
 
-    
+    -- challenge - 3
+UPDATE agents SET clearance_level = 'Level-5' WHERE agent_id =03;
+
+DELETE from agents WHERE agent_id=03;
+
+
+REPLACE INTO gadgets (gadget_id,gadget_name,assigned_agent_id) VALUES (003,'Bomb',01);
+
+
+select * from agents;
+
+-- challenge 4
+
+SELECT name,age from agents WHERE age BETWEEN 25 and 45;
+
+SELECT * FROM missions WHERE code_name IN ('elephant','lion','bear');
+
+SELECT * FROM agents WHERE NAME LIKE "%UL"
+
+
+delete from agents;
+delete from missions;
+
+delete from gadgets;
+
+INSERT INTO agents
+(agent_id, name, age, clearance_level, email)
+VALUES
+(101, 'Aarav Sharma', 29, 'Level-2', 'aarav@aegis.com'),
+(102, 'Priya Mehta', 34, 'Level-4', 'priya@aegis.com'),
+(103, 'Kabir Singh', 41, 'Level-5', 'kabir@aegis.com'),
+(104, 'Ananya Rao', 27, 'Level-3', 'ananya@aegis.com'),
+(105, 'Rohan Kapoor', 38, 'Level-4', 'rohan@aegis.com');
+
+
+
+INSERT INTO missions
+(mission_id, code_name, operation_cost, lead_agent_id)
+VALUES
+(201, 'Operation Falcon', 150000.00, 103),
+(202, 'Silent Storm', 95000.00, 102),
+(203, 'Black Panther', 220000.00, 101),
+(204, 'Desert Shadow', 180000.00, 105),
+(205, 'Iron Shield', 125000.00, 104);
+
+INSERT INTO gadgets
+(gadget_id, gadget_name, assigned_agent_id)
+VALUES
+(301, 'Night Vision Goggles', 101),
+(302, 'EMP Grenade', 103),
+(303, 'Hologram Projector', 102),
+(304, 'Tracking Drone', 105),
+(305, 'Laser Watch', 104),
+(306, 'Encrypted Communicator', 101);
+
+
+
+SELECT COUNT(*) AS total_agents FROM agents;
+
+SELECT AVG(operation_cost) AS Average_operation_cost FROM missions;
+
+SELECT MAX(operation_cost) AS highest_operation_cost FROM missions;
+
+
+SELECT
+    (SELECT COUNT(*) FROM agents) AS total_agents,
+    (SELECT AVG(operation_cost) FROM missions) AS average_operation_cost,
+    (SELECT MAX(operation_cost) FROM missions) AS highest_operation_cost;
+
+
+SELECT * FROM agents GROUP BY clearance_level HAVING COUNT(agent_id)>=1;
